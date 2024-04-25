b8d406187e753fe273662758272eaff13586b42a
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-04-25 19:36:04 -0400

    Declared JS constants for HTML elements

---
+++ script.js
@@ -0,0 +1,24 @@
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const pokeName = document.getElementById("pokemon-name");
+const pokeId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+const imgDiv = document.getElementById("img-div");
+const statBlock = document.getElementById("stat-block");
+
+async function fetchPokemon() {
+    try {
+
+    } catch (error) {
+
+    }
+
+}
