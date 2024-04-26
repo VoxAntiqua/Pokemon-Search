36435ddbe70b8668f7cc81c93bda671dd24058ea
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-04-25 21:58:24 -0400

    full functionality

--- script.js
+++ script.js
@@ -14,13 +14,31 @@ const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
 const imgDiv = document.getElementById("img-div");
 const statBlock = document.getElementById("stat-block");
+const basics = document.getElementById("basics");
 
 async function fetchPokemon() {
     try {
         const pokemonNameOrId = searchInput.value.toLowerCase().split(" ").join("-");
         const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`);
         const data = await response.json();
-        imgDiv.textContent = data;
+        // Unhide elements
+        statBlock.style.display = "block";
+        // Assign all values 
+        pokeName.textContent = data.name.toUpperCase();
+        pokeId.textContent = `#${data.id}`;
+        weight.textContent = `Weight: ${data.weight}`;
+        height.textContent = `Height: ${data.height}`;
+        hp.textContent = data.stats[0].base_stat;
+        attack.textContent = data.stats[1].base_stat;
+        defense.textContent = data.stats[2].base_stat;
+        specialAttack.textContent = data.stats[3].base_stat;
+        specialDefense.textContent = data.stats[4].base_stat;
+        speed.textContent = data.stats[5].base_stat;
+        // Assign types
+        types.innerHTML = data.types.map((obj) => `<span>${obj.type.name.toUpperCase()}</span>`)
+        .join(" ");
+        // Load image
+        imgDiv.innerHTML = `<img id="sprite" src="${data.sprites.front_default}" style="height: 25vh;"/>`
     } catch (error) {
         alert("Pokémon not found");
     }
@@ -28,5 +46,18 @@ async function fetchPokemon() {
 
 searchForm.addEventListener("submit", e => {
     e.preventDefault();
+    clearPage();
     fetchPokemon();
 });
+
+function clearPage() {
+    // Hide elements
+    statBlock.style.display = "none";
+    // Clear elements
+    types.innerHTML = "";
+    imgDiv.innerHTML = "";
+    pokeId.textContent = "";
+    pokeName.textContent = "";
+    weight.textContent = "";
+    height.textContent = "";
+}
